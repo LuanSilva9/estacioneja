7d0f1eb9abfe4a5d900566c36945eb08088c6f5e
LuanAlves17 <luan.alves@copasul.coop.br>
2025-04-27 23:07:18 -0300

    Adicionando Arquivos base

---
+++ src/test/java/br/com/estacioneja/EstacionejaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estacioneja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
